e854535449d380fc7363969eb49e5e4c6c087cd3
Dmitry <154436014+Sakatnex@users.noreply.github.com>
2025-01-12 13:10:41 +0300

    Add files via upload

---
+++ donbot/handlers/personal_actions.py
@@ -0,0 +1,84 @@
+import structlog
+from aiogram import Router, F, Bot
+from aiogram.exceptions import TelegramBadRequest
+from aiogram.filters import Command, CommandStart, CommandObject
+from aiogram.types import Message, LabeledPrice, PreCheckoutQuery, InlineKeyboardMarkup, CallbackQuery
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from fluent.runtime import FluentLocalization
+
+router = Router()
+router.message.filter(F.chat.type == "private")
+logger = structlog.get_logger()
+@router.message(Command("start"))
+async def cmd_owner_hello(message: Message, l10n: FluentLocalization):
+    await message.answer(l10n.format_value("hello-msg"))
+
+@router.message(Command("donate", "donat", "донат"))
+async def cmd_donate(message: Message, command: CommandObject, l10n: FluentLocalization):
+    if command.args is None or not command.args.isdigit() or not 1 <= int(command.args) <= 2500:
+        await message.answer(l10n.format_value("donate-input-error"))
+        return
+    amount = int(command.args)
+    kb = InlineKeyboardBuilder()
+    kb.button(
+        text=l10n.format_value("donate-button-pay", {"amount": amount}),
+        pay=True # важный параметр!
+    )
+    kb.button(
+        text=l10n.format_value("donate-button-cancel"),
+        callback_data="donate_cancel"
+    )
+    kb.adjust(1)
+    prices = [LabeledPrice(label="XTR", amount=amount)]
+    await message.answer_invoice(
+        title=l10n.format_value("donate-invoice-title"),
+        description=l10n.format_value("donate-invoice-description", {"amount": amount}),
+        prices=prices,
+        provider_token="",
+        payload=f"{amount}_stars",
+        currency="XTR",
+        reply_markup=kb.as_markup()
+    )
+
+@router.callback_query(F.data == "donate_cancel")
+async def on_donate_cancel(callback: CallbackQuery, l10n: FluentLocalization):
+    await callback.answer(l10n.format_value("donate-cancel-payment"))
+
+    await callback.message.delete()
+
+@router.message(Command("paysupport"))
+async def cmd_paysupport(message: Message, l10n: FluentLocalization):
+    await message.answer(l10n.format_value("donate-paysupport-message"))
+
+@router.message(Command("refund"))
+async def cmd_refund(message: Message, bot: Bot, command: CommandObject, l10n: FluentLocalization):
+    t_id = command.args
+    if t_id is None:
+        await message.answer(l10n.format_value("donate-refund-input-error"))
+        return
+    try:
+        await bot.refund_star_payment(
+            user_id=message.from_user.id,
+            telegram_payment_charge_id=t_id
+        )
+        await message.answer(l10n.format_value("donate-refund-success"))
+    except TelegramBadRequest as e:
+        err_text = l10n.format_value("donate-refund-code-not-found")
+        if "CHARGE_ALREADY_REFUNDED" in e.message:
+            err_text = l10n.format_value("donate-refund-already-refunded")
+        await message.answer(err_text)
+        return
+
+@router.pre_checkout_query()
+async def pre_checkout_query(query: PreCheckoutQuery, l10n: FluentLocalization):
+    await query.answer(ok=True)
+
+@router.message(F.successful_payment)
+async def on_successfull_payment(message: Message, l10n: FluentLocalization):
+    await message.answer(
+        l10n.format_value(
+            "donate-successful-payment",
+            {"t_id": message.successful_payment.telegram_payment_charge_id}
+        ),
+        message_effect_id="5104841245755180586",
+    )
