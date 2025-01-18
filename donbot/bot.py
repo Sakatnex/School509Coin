4135d90da161b71e87a34b3c605c45c33d8ba160
Dmitry <154436014+Sakatnex@users.noreply.github.com>
2025-01-18 17:34:35 +0300

    Update bot.py

--- donbot/bot.py
+++ donbot/bot.py
@@ -13,28 +13,23 @@ from dispatcher import dp
 import handlers
 
 async def main():
-    # init logging
     log_config: LogConfig = get_config(model=LogConfig, root_key="logs")
     structlog.configure(**get_structlog_config(log_config))
 
-    # get bot config
     bot_config: BotConfig = get_config(model=BotConfig, root_key="bot")
 
-    # init bot object
     bot = Bot(
-        token=bot_config.token.get_secret_value(), # get token as secret, so it will be hidden in logs
+        token=bot_config.token.get_secret_value(), 
         default=DefaultBotProperties(
-            parse_mode=ParseMode.HTML # ParseMode (HTML or MARKDOWN_V2 is preferable)
+            parse_mode=ParseMode.HTML 
         )
     )
 
-    # start the logger
     logger: FilteringBoundLogger = structlog.get_logger()
     await logger.ainfo("Starting the bot...")
 
-    # start polling
     try:
-        await dp.start_polling(bot, skip_updates=False) # Don't skip updates, if your bot will process payments or other important stuff
+        await dp.start_polling(bot, skip_updates=False) 
     finally:
         await bot.session.close()
 
