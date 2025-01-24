7ab2d5f1cd7efac00d63ef64ea861c38a27672ba
Dmitry <154436014+Sakatnex@users.noreply.github.com>
2025-01-24 15:11:07 +0300

    Update admin_actions.py

--- donbot/handlers/admin_actions.py
+++ donbot/handlers/admin_actions.py
@@ -8,22 +8,17 @@ from fluent.runtime import FluentLocalization
 
 from filters.is_owner import IsOwnerFilter
 
-# Declare router
 router = Router()
-router.message.filter(F.chat.type == "private", IsOwnerFilter(is_owner=True)) # allow bot admin actions only for bot owner
+router.message.filter(F.chat.type == "private", IsOwnerFilter(is_owner=True)) 
 
-# Declare handlers
 logger = structlog.get_logger()
 
-# Handlers:
 @router.message(Command("start"))
 async def cmd_owner_hello(message: Message, l10n: FluentLocalization):
     await message.answer(
         l10n.format_value("hello-owner")
     )
 
-
-# Here is some example !ping command ...
 @router.message(
     IsOwnerFilter(is_owner=True),
     Command(commands=["ping"]),
